c83b004fd67a4f3f5b54df56d2ecec1590b2c569
Unknown <dreaddrone@gmail.com>
2018-04-20 12:42:21 +0200

    working project with bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AppComponent } from './app.component';
 import { TestModule } from './test-module/test.module';
 import { CharacterService } from './services/marvel.characters.service';
+import { DeLijnComponent } from './de-lijn/de-lijn.component';
+import { DeLijnService } from './services/delijn.service';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
@@ -15,10 +19,13 @@ import { CharacterService } from './services/marvel.characters.service';
   imports: [
     BrowserModule,
     MDBBootstrapModule.forRoot(),
-    
+    FormsModule,
+    HttpClientModule
   ],
   schemas: [NO_ERRORS_SCHEMA],
-  providers: [CharacterService],
+  providers: [
+    CharacterService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
