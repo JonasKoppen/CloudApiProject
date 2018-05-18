9be93615477c33022815fa277d4ae4b04bd106b7
Unknown <dreaddrone@gmail.com>
2018-05-18 11:17:16 +0200

    filter en sorteer opties Movies
    filter by titel, phase
    sorteer by titel, IMDB score, phase en watchorder
    (ascending en descinding)
    
    plain text met film info in de Movie object gestoken

--- dotNet/MarvelMoviesAPI/MarvelMoviesAPI/Controllers/MovieController.cs
+++ dotNet/MarvelMoviesAPI/MarvelMoviesAPI/Controllers/MovieController.cs
@@ -18,10 +18,52 @@ namespace MarvelMoviesAPI.Controllers
         }
         
         [HttpGet]
-        public List<Movie> GetMovies()
+        public List<Movie> GetMovies(string title, int? phase, string sort, string dir = "asc")
         {
-            var movies = context.MarvelMovies.Include(v => v.Villain).Include(h => h.Hero).ToList();
-            return movies;
+            
+            IQueryable<Movie> query = context.MarvelMovies;
+            if (!string.IsNullOrWhiteSpace(title))
+                query = query.Where(m => m.Title == title);
+            if (phase > 0)
+                query = query.Where(m => m.Phase == phase);
+            if (!string.IsNullOrWhiteSpace(sort))
+            {
+                switch (sort)
+                {
+                    case "title":
+                        if (dir == "asc")
+                            query = query.OrderBy(m => m.Title);
+                        else if (dir == "desc")
+                            query = query.OrderByDescending(m => m.Title);
+                        break;
+                    case "IMDBScore":
+                        if (dir == "asc")
+                            query = query.OrderBy(m => m.IMDBScore);
+                        else if (dir == "desc")
+                            query = query.OrderByDescending(m => m.IMDBScore);
+                        break;
+                    case "ReleaseYear":
+                        if (dir == "asc")
+                            query = query.OrderBy(m => m.ReleaseYear);
+                        else if (dir == "desc")
+                            query = query.OrderByDescending(m => m.ReleaseYear);
+                        break;
+                    case "Phase":
+                        if (dir == "asc")
+                            query = query.OrderBy(m => m.Phase);
+                        else if (dir == "desc")
+                            query = query.OrderByDescending(m => m.Phase);
+                        break;
+                    case "TimeLineOrder":
+                        if (dir == "asc")
+                            query = query.OrderBy(m => m.TimeLineOrder);
+                        else if (dir == "desc")
+                            query = query.OrderByDescending(m => m.TimeLineOrder);
+                        break;
+                }
+            }
+            return query.Include(v => v.Villain).Include(h => h.Hero).ToList();
         }
+   
     }
 }
