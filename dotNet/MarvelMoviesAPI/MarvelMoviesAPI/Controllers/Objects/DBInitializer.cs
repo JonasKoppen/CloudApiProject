a84a330faeec3a4940b05023c9df88d8f6942fef
Unknown <dreaddrone@gmail.com>
2018-05-25 14:19:29 +0200

    DB update
    DB zal bij een clean boot meer items bevatten

--- dotNet/MarvelMoviesAPI/MarvelMoviesAPI/Controllers/Objects/DBInitializer.cs
+++ dotNet/MarvelMoviesAPI/MarvelMoviesAPI/Controllers/Objects/DBInitializer.cs
@@ -19,7 +19,14 @@ namespace MarvelMoviesAPI.Controllers.Objects
                         {
                             Name = "Tony Stark",
                             HeroName = "Iron Man",
-                            Actor = "Robert Downey Jr.",
+                            Actor = "Robert Downey Jr."
+                            //FeaturedMovies = context.MarvelMovies.Where(m => m.Hero.Name == "Tony Stark").ToList() //hero.id
+                        },
+                    new Hero()
+                        {
+                            Name = "Thor Odinson",
+                            HeroName = "Thor",
+                            Actor = "Chris Hemsworth"
                             //FeaturedMovies = context.MarvelMovies.Where(m => m.Hero.Name == "Tony Stark").ToList() //hero.id
                         }
                 };
@@ -44,6 +51,21 @@ namespace MarvelMoviesAPI.Controllers.Objects
                     {
                         Name = "Aldrich Killian",
                         Actor = "Guy Pearce",
+                    },
+                    new Villain()
+                    {
+                        Name = "Loki Laufeyson",
+                        Actor = "Tom Hiddleston",
+                    },
+                    new Villain()
+                    {
+                        Name = "Malekith",
+                        Actor = "Christopher Eccleston",
+                    },
+                    new Villain()
+                    {
+                        Name = "Hela",
+                        Actor = "Cate Blanchett",
                     }
                 };
                 foreach (Villain v in villains)
@@ -85,6 +107,41 @@ namespace MarvelMoviesAPI.Controllers.Objects
                     Villain = villains[2],
                     Phase = 2,
                     TimeLineOrder = 7
+                };
+                context.MarvelMovies.Add(mv);
+                mv = new Movie()
+                {
+                    Title = "Thor",
+                    ReleaseYear = 2011,
+                    Director = "Kenneth Branagh",
+                    IMDBScore = 7.0f,
+                    Hero = heroes[1],
+                    Villain = villains[3],
+                    Phase = 1,
+                    TimeLineOrder = 5
+                };
+                mv = new Movie()
+                {
+                    Title = "Thor: The Dark World",
+                    ReleaseYear = 2013,
+                    Director = " Alan Taylor ",
+                    IMDBScore = 7.0f,
+                    Hero = heroes[1],
+                    Villain = villains[4],
+                    Phase = 2,
+                    TimeLineOrder = 8
+                };
+                context.MarvelMovies.Add(mv);
+                mv = new Movie()
+                {
+                    Title = "Thor: Ragnarok",
+                    ReleaseYear = 2017,
+                    Director = "Taika Waititi",
+                    IMDBScore = 7.9f,
+                    Hero = heroes[1],
+                    Villain = villains[5],
+                    Phase = 3,
+                    TimeLineOrder = 17
                 };
                 context.MarvelMovies.Add(mv);
                 context.SaveChanges();
